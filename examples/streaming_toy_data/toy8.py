3287e58a6e436048b9cdff97f856f92dabb0a779
Steve Schmerler <git@elcorto.com>
2023-01-31 17:05:58 +0100

    Cleanup toy8 data generation functions
    Add type annotations for minimal documentation.
    
    Rename generate_timeDependent() -> generate_time_dependent(), keep
    legacy alias.
    
    Remove unused code in generate_time_dependent().
    
    Use new recommended numpy default_rng in generate(). Use
    rng.normal(loc=...) in loop.
    
    Add sanity check in generate().
    
    style: black -l79, flake8

--- examples/streaming_toy_data/toy8.py
+++ examples/streaming_toy_data/toy8.py
@@ -1,70 +1,72 @@
-import numpy as np
-import torch
+from typing import Callable
 
-def generate(labels, tot_dataset_size):
-    # print('Generating artifical data for setup "%s"' % (labels))
+import numpy as np
+import torch as T
+
+
+def generate(labels: str, tot_dataset_size: int) -> tuple[T.Tensor, T.Tensor]:
+
+    assert tot_dataset_size % 8 == 0, "tot_dataset_size is not a multiple of 8"
 
     verts = [
-         (-2.4142, 1.),
-         (-1., 2.4142),
-         (1.,  2.4142),
-         (2.4142,  1.),
-         (2.4142, -1.),
-         (1., -2.4142),
-         (-1., -2.4142),
-         (-2.4142, -1.)
-        ]
+        (-2.4142, 1.0),
+        (-1.0, 2.4142),
+        (1.0, 2.4142),
+        (2.4142, 1.0),
+        (2.4142, -1.0),
+        (1.0, -2.4142),
+        (-1.0, -2.4142),
+        (-2.4142, -1.0),
+    ]
 
     label_maps = {
-                  'all':  [0, 1, 2, 3, 4, 5, 6, 7],
-                  'some': [0, 0, 0, 0, 1, 1, 2, 3],
-                  'none': [0, 0, 0, 0, 0, 0, 0, 0],
-                 }
-    
-    np.random.seed(0)
+        "all": [0, 1, 2, 3, 4, 5, 6, 7],
+        "some": [0, 0, 0, 0, 1, 1, 2, 3],
+        "none": [0, 0, 0, 0, 0, 0, 0, 0],
+    }
+
+    rng = np.random.default_rng(seed=0)
     N = tot_dataset_size
     mapping = label_maps[labels]
 
-    pos = np.random.normal(size=(N, 2), scale=0.2)
-    labels = np.zeros((N, 8))
-    n = N//8
+    pos = np.empty((N, 2))
+    onehot_labels = np.zeros((N, 8))
+    n = N // 8
 
     for i, v in enumerate(verts):
-        pos[i*n:(i+1)*n, :] += v
-        labels[i*n:(i+1)*n, mapping[i]] = 1.
-   
-    pos = torch.tensor(pos, dtype=torch.float)
-    labels = torch.tensor(labels, dtype=torch.float)
+        pos[i * n : (i + 1) * n, :] = rng.normal(loc=v, scale=0.2, size=(n, 2))
+        onehot_labels[i * n : (i + 1) * n, mapping[i]] = 1.0
 
-    return pos, labels
+    return T.tensor(pos, dtype=T.float), T.tensor(onehot_labels, dtype=T.float)
 
-def generate_timeDependent(labels, tot_dataset_size, fctn, t = np.arange(10)):
 
-    verts = [
-         (-2.4142, 1.),
-         (-1., 2.4142),
-         (1.,  2.4142),
-         (2.4142,  1.),
-         (2.4142, -1.),
-         (1., -2.4142),
-         (-1., -2.4142),
-         (-2.4142, -1.)
-        ]
+def generate_time_dependent(
+    labels: str,
+    tot_dataset_size: int,
+    fctn: Callable,
+    t: np.ndarray = np.arange(10),
+) -> tuple[T.Tensor, T.Tensor]:
 
-    label_maps = {
-                  'all':  [0, 1, 2, 3, 4, 5, 6, 7],
-                  'some': [0, 0, 0, 0, 1, 1, 2, 3],
-                  'none': [0, 0, 0, 0, 0, 0, 0, 0],
-                 }
-    
-    np.random.seed(0)
-    
-    pos, labels = generate(labels, tot_dataset_size)
+    """
+    Parameters
+    ----------
+    labels, tot_dataset_size :
+        See generate()
+    fctn :
+        Map scalar time to length-2 array of additive adjustments for
+        positions.
+    t : (n_time,)
+        Time axis.
+    """
 
-    pos_t, labels_t = pos, labels
-    
+    pos, onehot_labels = generate(labels, tot_dataset_size)
+    pos_t, onehot_labels_t = pos, onehot_labels
+
     for ti in t:
-        pos_t = torch.cat([pos_t, pos+fctn(ti)])
-        labels_t = torch.cat([labels_t, (ti+1)*labels]) # 
+        pos_t = T.cat([pos_t, pos + fctn(ti)])
+        onehot_labels_t = T.cat([onehot_labels_t, (ti + 1) * onehot_labels])
 
-    return pos_t, labels_t
+    return pos_t, onehot_labels_t
+
+
+generate_timeDependent = generate_time_dependent
