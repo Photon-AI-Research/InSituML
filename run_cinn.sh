915aa97186030fa66a7a1cebd10b2ec443738ba1
ebert1 <ja.ebert@fz-juelich.de>
2023-02-02 18:17:53 +0100

    Automatically create checkpoint directory

--- run_cinn.sh
+++ run_cinn.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 
+cp_dir=./checkpoints
+mkdir -p "$cp_dir"
+
 # Set `lr=0` to use learning rate from config.
 lr=0
 python3 main/Application.py \
-        --modelPath ./checkpoints \
+        --modelPath "$cp_dir" \
         --datasetName pc_field \
         --nTasks 1 \
         --batchSize 1 \
