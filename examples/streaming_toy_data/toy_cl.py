aab5ec6d01d9465ff92e5a539a6bc39373936bdf
Steve Schmerler <git@elcorto.com>
2023-06-07 15:18:07 +0200

    toy cl: bug: fix loss plotting, add train_and_plot.py
    Plotting: we used to remove the min from each loss section (each time step) and
    shifted along y in order to plot on a log scale. That lead to an error in how
    we plot the loss: different converged loss values (esp. when using ER) were not
    visible.
    
    Increase plot font size. Add axes labels.
    
    Rename simple_train.py -> toy_cl.py . Add train() and plot().
    
    Add support for GPU training.
    
    Enhanced API: You can still use ./toy_cl.py to run a train + plotting. Plots
    are now saved to disk instead of shown (assume remote cluster execution), as
    will be train data relevant for plotting as pickle file. But we recommend
    calling functions defined in toy_cl.py . See train_and_plot.py .

--- examples/streaming_toy_data/toy_cl.py
+++ examples/streaming_toy_data/toy_cl.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 
 from typing import Sequence
-from multiprocessing import cpu_count
 
 from icecream import ic
-
 from torch.utils.data import DataLoader
 import torch as T
 from torch import nn
-
-import numpy as np
-
+import dill as pickle
 from matplotlib import pyplot as plt
+import numpy as np
 
 from FrEIA.framework import (
     InputNode,
@@ -100,47 +97,49 @@ def build_model(
     return model
 
 
-def plot_chunks(
-    ax,
-    lst: Sequence[Sequence[float]],
-    plot_kwds=dict(),
-    vlines_kwds=dict(colors="gray", linestyles="--"),
-    log_y: bool = False,
-):
-    line_pos = []
-    start = 0
-    plot = ax.semilogy if log_y else ax.plot
-    for y in lst:
-        x = start + np.arange(len(y))
-        plot(x, y, **plot_kwds)
-        start = x[-1]
-        line_pos.append(start)
-    ax.vlines(line_pos[:-1], *ax.yaxis.get_data_interval(), **vlines_kwds)
+def train(use_mem=False):
+    """
+    Parameters
+    ----------
+    use_mem : Use memory mechanism to counteract "forgetting"
+    """
 
+    data_file = f"toy_cl_mem_{use_mem}.pk"
 
-if __name__ == "__main__":
-
-    ##T.set_num_threads(cpu_count() // 2)
+    use_cuda = T.cuda.is_available()
+    device = "cuda" if use_cuda else "cpu"
+    ic(device)
+    ic(use_mem)
 
     # Manually switch on/off convergence control
     conv_control = True
 
-    # Use memory mechanism to counteract "forgetting"
-    use_mem = True
-
     if conv_control:
         assert conv_control_possible
 
-    nsteps = 10
+    # Production settings
+    nsteps = 20
     npoints = 2048
-    batch_size_div = 4
-    batch_size = max(npoints // batch_size_div, 1)
-    print_every_epoch = 50
+    print_every_epoch = 10
     max_epoch = int(1e4) if conv_control else int(1e3)
 
+    # For testing
+    ##nsteps = 3
+    ##npoints = 128
+    ##print_every_epoch = 1
+    ##max_epoch = 3
+
+    batch_size_div = 4
+    batch_size = max(npoints // batch_size_div, 1)
+    seed = 123
+    dt = 0.3
+    label_kind = "all"
+
     ds = generate.TimeDependentTensorDataset(
-        *generate.generate_toy8(label_kind="all", npoints=npoints, seed=123),
-        dt=0.3,
+        *generate.generate_toy8(
+            label_kind=label_kind, npoints=npoints, seed=seed
+        ),
+        dt=dt,
     )
 
     train_dl = DataLoader(
@@ -150,6 +149,10 @@ if __name__ == "__main__":
     model = build_model(
         ndim_x=2, ndim_y=8, n_coupling=4, hidden_width=512, n_hidden=1
     )
+
+    if use_cuda:
+        model = model.to(device)
+
     trainable_parameters = [p for p in model.parameters() if p.requires_grad]
     optimizer = T.optim.AdamW(
         trainable_parameters,
@@ -170,20 +173,27 @@ if __name__ == "__main__":
 
     # PIC time step
     for i_step in range(nsteps):
-        # train some epochs on this time step's data
+        # Train some epochs on this time step's data, until convergence if
+        # conv_control is used.
         i_epoch = -1
         mean_epoch_loss_hist = []
         if conv_control:
-            conv = SlopeZero(wlen=25, tol=1e-4, wait=10, reduction=np.mean)
+            conv = SlopeZero(wlen=25, tol=1e-4, wait=20, reduction=np.mean)
         while True:
             i_epoch += 1
             loss_sum = 0.0
             for i_batch, (X_batch, Y_batch) in enumerate(train_dl):
+                if use_cuda:
+                    X_batch = X_batch.to(device)
+                    Y_batch = Y_batch.to(device)
 
                 # Don't use memory in first step since there is nothing to
                 # remember.
                 if use_mem and i_step > 0:
                     X_batch_mem, Y_batch_mem = er_mem.sample(batch_size)
+                    if use_cuda:
+                        X_batch_mem = X_batch_mem.to(device)
+                        Y_batch_mem = Y_batch_mem.to(device)
                     Xb = T.vstack((X_batch, X_batch_mem))
                     Yb = T.vstack((Y_batch, Y_batch_mem))
                 else:
@@ -232,13 +242,71 @@ if __name__ == "__main__":
         if use_mem:
             ic(er_mem.status())
 
+    # Bring model back to CPU before saving to cache.
+    if use_cuda:
+        model = model.to("cpu")
+
+    data = dict(
+        label_kind=label_kind,
+        dt=dt,
+        seed=seed,
+        model=model,
+        nsteps=nsteps,
+        npoints=npoints,
+        loss_hist=loss_hist,
+    )
+
+    pickle.dump(data, open(data_file, "wb"))
+    return data_file
+
+
+def plot_chunks(
+    ax,
+    lst: Sequence[Sequence[float]],
+    plot_kwds=dict(),
+    vlines_kwds=dict(colors="gray", linestyles="--", alpha=0.5),
+    log_y: bool = False,
+):
+    line_pos = []
+    start = 0
+    plot = ax.semilogy if log_y else ax.plot
+    for y in lst:
+        x = start + np.arange(len(y))
+        plot(x, y, **plot_kwds)
+        start = x[-1]
+        line_pos.append(start)
+    ##ymin, ymax = ax.yaxis.get_data_interval()
+    ymin, ymax = ax.get_ylim()
+    ax.vlines(line_pos[:-1], ymin, ymax, **vlines_kwds)
+    # Fix changed ylim bottom from adding vlines ... sigh.
+    ax.set_ylim(bottom=ymin)
+
+
+def plot(data_file: str = None):
+    data = pickle.load(open(data_file, "rb"))
+
+    plt.rcParams["font.size"] = 15
+
+    npoints = data["npoints"]
+    nsteps = data["nsteps"]
+    model = data["model"]
+    loss_hist = data["loss_hist"]
+
+    # Re-create train data for plotting
+    ds = generate.TimeDependentTensorDataset(
+        *generate.generate_toy8(
+            label_kind=data["label_kind"], npoints=npoints, seed=data["seed"]
+        ),
+        dt=data["dt"],
+    )
+    # Should be default, but just to make sure
+    ds.time = 0
+
     ncols = 3
     fig, axs = plt.subplots(
         ncols=ncols, figsize=(5 * ncols, 5), tight_layout=True
     )
 
-    # Reset time, re-create train data for plotting
-    ds.time = 0
     Xt_3d, Yt_3d = generate.tdds_arrays(
         ds=ds, nsteps=nsteps, batch_size=npoints
     )
@@ -251,13 +319,21 @@ if __name__ == "__main__":
         rev_X, _ = model(T.randn(Xt.shape), c=T.from_numpy(Yt), rev=True)
 
         axs[1].scatter(rev_X[:, 0], rev_X[:, 1], c=color, s=0.5)
+        plot_chunks(axs[2], loss_hist, log_y=False)
 
-        loss_hist_shifted = [x + np.abs(np.min(x)) + 1 for x in loss_hist]
-        plot_chunks(axs[2], loss_hist_shifted, log_y=True)
-        ##plot_chunks(axs[2], loss_hist, log_y=False)
+    for ax in axs[:2]:
+        ax.set_xlabel("$x_0$")
+        ax.set_ylabel("$x_1$")
 
     axs[2].set_xlabel("epoch")
     axs[2].set_ylabel("mean epoch loss")
-    axs[2].set_ylim(top=1.005)
+    axs[2].set_ylim(top=0.0)
 
-    plt.show()
+    for ext in ["png"]:
+        fig_fn = data_file.replace(".pk", f".{ext}")
+        fig.savefig(fig_fn)
+
+
+if __name__ == "__main__":
+    data_file = train(use_mem=False)
+    plot(data_file)
