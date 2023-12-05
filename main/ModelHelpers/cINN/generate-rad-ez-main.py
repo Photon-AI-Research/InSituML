8cfc8a65a621a291b00d38cee0260928a7788c05
Jeyhun1 <78599941+Jeyhun1@users.noreply.github.com>
2023-12-06 00:55:18 +0100

    Update generate-rad-ez-main.py

--- main/ModelHelpers/cINN/generate-rad-ez-main.py
+++ main/ModelHelpers/cINN/generate-rad-ez-main.py
@@ -6,14 +6,11 @@ import os
 from radiation import RadiationData
 
 
-
 filename2= "/bigdata/hplsim/production/KHI_for_GB_MR/runs/002_KHI_withRad_randomInit/simOutput/openPMD/simData_000000.bp"
-print(filename2)
 series2 = io.Series(filename2, io.Access_Type.read_only)
 
 ii = series2.iterations[0]
 particles = ii.particles["e_all"]
-
 
 h1 = particles.particle_patches["extent"]
 
@@ -34,10 +31,8 @@ r_offset = np.empty((len(extent_x), 3))
 r_offset[:, 0] = offset_x * ii.get_attribute("cell_width")
 r_offset[:, 1] = offset_y * ii.get_attribute("cell_height")
 r_offset[:, 2] = offset_z * ii.get_attribute("cell_depth")
-print(r_offset)
 
 filename = "/bigdata/hplsim/production/KHI_for_GB_MR/runs/002_KHI_withRad_randomInit/simOutput/radiationOpenPMD/e_radAmplitudes%T.bp"
-print(filename)
 series = io.Series(filename, io.Access_Type.read_only)
 
 for iteration in range(2,2001):
@@ -59,23 +54,17 @@ for iteration in range(2,2001):
     DetectorFrequency = DetectorFrequency["omega"][0, :, 0]
     series.flush()
     
-    print(Dist_Amplitude_x.max())
     n_vec = np.empty((len(DetectorDirection_x), 3))
     n_vec[:, 0] = DetectorDirection_x
     n_vec[:, 1] = DetectorDirection_y
     n_vec[:, 2] = DetectorDirection_z
-    print('n_vec', n_vec.shape)
-    
+
+    # time retardation correction
     phase_offset = np.exp(-1.j * DetectorFrequency[np.newaxis, np.newaxis, :]*  (iteration + np.dot(r_offset, n_vec.T)[:, :, np.newaxis] / 1.0))
-    print('phase_offset', phase_offset.shape)
     
     Dist_Amplitude_offset_x = (Dist_Amplitude_x / phase_offset)
     Dist_Amplitude_offset_y = (Dist_Amplitude_y / phase_offset)
     Dist_Amplitude_offset_z = (Dist_Amplitude_z / phase_offset)
-    
-    print('Dist_Amplitude_offset_x', Dist_Amplitude_offset_x.shape)
-    print('Dist_Amplitude_offset_y', Dist_Amplitude_offset_y.shape)
-    print('Dist_Amplitude_offset_z', Dist_Amplitude_offset_z.shape)
     
     #index = -1 to get ex vector = [0,0,-1]
     index = -1
@@ -85,9 +74,7 @@ for iteration in range(2,2001):
 
     # Concatenate along the second axis
     amplitude_concat = np.stack((amplitude_x, amplitude_y, amplitude_z), axis=1)
-    
-    print('amplitude_concat',amplitude_concat.shape)
-    
+        
     file_rad_new = '/bigdata/hplsim/aipp/Jeyhun/khi/part_rad/radiation_002_ez/' + str(iteration)+'.npy'
 
     if os.path.exists(file_rad_new):
