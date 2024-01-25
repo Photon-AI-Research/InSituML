5a29477b5f2c376cc90261f48218a61008ea809c
Ankush7890 <ankushc48@gmail.com>
2024-01-25 12:00:12 +0100

    startJobs

--- main/ModelHelpers/cINN/train_khi_AE_refactored/start_jobs.sh
+++ main/ModelHelpers/cINN/train_khi_AE_refactored/start_jobs.sh
@@ -1,9 +1,11 @@
 property=("positions" "momentum" "force" "all")
+property=("positions" "momentum")
 
 for property in ${property[@]}
 do
 
 mkdir $property
+cd $property
 
 jobname="Training"$property
 
@@ -15,17 +17,20 @@ echo "#SBATCH --ntasks-per-node=1">>job.sh
 echo "#SBATCH --cpus-per-task=1">>job.sh
 echo "#SBATCH --account=casus">>job.sh
 echo "#SBATCH --partition=casus">>job.sh
+echo "#SBATCH --gres=gpu:1">>job.sh
 echo "#SBATCH --time=12:00:00">>job.sh
 
+echo "module load python cuda gcc">>job.sh
+echo "source /home/checkr99/.new_env3.10/bin/activate">>job.sh
 echo "">>job.sh
 
 
-CALL_SEQUENCE=" /home/checkr99/InSituML/main/ModelHelpers/cINN/train_khi_AE_refactored/trainer.py "
+CALL_SEQUENCE="python3 /home/checkr99/InSituML/main/ModelHelpers/cINN/train_khi_AE_refactored/main.py "
 CALL_SEQUENCE+=" --property_ "$property
 
 echo $CALL_SEQUENCE>>job.sh
 
-sbatch job.sh
+#sbatch job.sh
 
 cd ..
 done
