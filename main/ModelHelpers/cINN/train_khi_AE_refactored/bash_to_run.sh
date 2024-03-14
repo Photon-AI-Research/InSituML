0475aed97913fb6b9e5f85204e15d1f4a81c794a
Ankush7890 <ankushc48@gmail.com>
2024-03-14 09:09:00 +0100

    bash with more dic items

--- main/ModelHelpers/cINN/train_khi_AE_refactored/bash_to_run.sh
+++ main/ModelHelpers/cINN/train_khi_AE_refactored/bash_to_run.sh
@@ -1,5 +1,4 @@
 cat << EOF > pyscript.py
-from collections import OrderedDict
 import numpy as np
 import itertools
 import os
@@ -16,6 +15,17 @@ slurm_content += "#SBATCH --gres=gpu:1\n"
 slurm_content += "#SBATCH --time=48:00:00\n"
 slurm_content += "module load python cuda gcc\n"
 slurm_content += "source /home/checkr99/.new_env3.10/bin/activate\n\n"
+
+
+input_command_def = "python3 /home/checkr99/InSituML/main/ModelHelpers/cINN/train_khi_AE_refactored/main.py "
+
+input_command_def += " --property_ momentum_force "
+input_command_def += " --learning_rate 1e-3 "
+input_command_def += " --num_epochs 15 "
+input_command_def += " --lossfunction earthmovers "
+input_command_def += " --ae_config non_deterministic "
+input_command_def += " --particles_to_sample 150000 "
+input_command_def += " --project_kw momentum_force_emd "
 
 def generate_kwargs(hyperparam_dic):
 
@@ -36,19 +46,51 @@ def generate_kwargs(hyperparam_dic):
     
     return kwargs_out
 
-hp_dic_format = {'learning_rate':[1e-3, 1e-4],
-                 'z_dim':[1024, 2048],
-                 'network':["VAE", "conAE"]}
+
+encoder_kwargs = {'ae_config':'non_deterministic',
+                  'fc_layer_config':'[256]',
+                  'conv_add_bn':True}
+                  
+decoder_kwargs = {'initial_conv3d_size':'[16, 4, 4, 4]',
+                  'add_batch_normalisation':True}
+
+def extract_key(key, value):
+    s="""" """
+    
+    if 'encoder_kwargs' in key:
+        
+        kys = key.split("::")[1]
+        encoder_kwargs[kys] = value
+        return 'encoder_kwargs', s + str(encoder_kwargs) + s
+        
+    if 'decoder_kwargs' in key:
+        
+        kys = key.split("::")[1]
+        decoder_kwargs[kys] = value
+        return 'decoder_kwargs',s + str(decoder_kwargs) + s
+    
+    return k, v
+
+hp_dic_format = {'encoder_kwargs::conv_layer_config':['[16, 32, 64, 128, 256, 512]',
+                                                      '[128, 256, 512, 1024]',
+                                                      '[128, 256, 512, 1024, 2048]'],
+                                      
+                 'decoder_kwargs::initial_conv3d_size':['[16, 4, 4, 4]',
+                                                        '[8, 8, 4, 4]',
+                                                        '[8, 4, 8, 4]']
+                 }
 
 kwargs_out = generate_kwargs(hp_dic_format)
 
 for kwgs_dic in kwargs_out:
-    input_command = "python3 main.py "
+    input_command = ""
     dir_name = ""
     for k, v in kwgs_dic.items():
-        input_command += f" --{k} {v} "
         dir_name += f"{k}_{v}"
-    
+        k, v = extract_key(k,v)
+        input_command += f" --{k} {v} "
+        
+    input_command = input_command_def + input_command
     os.mkdir(dir_name)
     f = open(f"{dir_name}/job.sh", "w")
     f.write(slurm_content.format(jobname="Training", partition_name="casus"))
