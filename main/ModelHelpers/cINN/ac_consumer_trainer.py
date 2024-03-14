e819552a07bd128d76087188e5c8f7f80ad7ad82
Ankush7890 <ankushc48@gmail.com>
2024-03-14 15:30:48 +0100

    simple initial additions

--- main/ModelHelpers/cINN/ac_consumer_trainer.py
+++ main/ModelHelpers/cINN/ac_consumer_trainer.py
@@ -5,6 +5,9 @@
 import time
 from threading import Thread
 import torch
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.distributed as dist
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class ModelTrainer(Thread):
@@ -38,6 +41,7 @@ class ModelTrainer(Thread):
                  model, 
                  optimizer,
                  scheduler,
+                 multigpu_run=True,
                  sleep_before_retry=10,
                  ts_after_stopped_production=10,
                  logger=None):
@@ -49,14 +53,28 @@ class ModelTrainer(Thread):
         self.model = model
         self.optimizer = optimizer
         self.scheduler = scheduler
-
         self.cumulative_loss = 0.0
         self.sleep_before_retry = sleep_before_retry
         self.logger = logger
         
         self.batch_passes = 0
         self.ts_after_stopped_production = ts_after_stopped_production
-        
+
+        if multigpu_run is not None:
+
+            self.multigpu_run = multigpu_run
+
+            dist.init_process_group("nccl")
+
+            self.gpu_id = dist.get_rank()
+
+            torch.cuda.set_device(self.gpu_id)
+            torch.cuda.empty_cache()
+
+            self.model = DDP(self.model, device_ids=[self.gpu_id])
+        else:
+            self.gpu_id = device
+
     def run(self):
 
         rest_training_left_counter=0
@@ -71,16 +89,17 @@ class ModelTrainer(Thread):
             #for training to begin
             if phase_space_radiation is None:
                 print(f"Trainer will wait for {self.sleep_before_retry} seconds, for data to be "
-                        "streamed before reattempting batch extraction." )
+                        f"streamed before reattempting batch extraction." )
                 time.sleep(self.sleep_before_retry)
                 continue   
             
             phase_space, radiation = phase_space_radiation
             
-            phase_space = phase_space.to(device)
-            radiation = radiation.to(device)
-            
-            if self.batch_passes != 0:
+            phase_space = phase_space.to(self.gpu_id)
+            radiation = radiation.to(self.gpu_id)
+
+            # only logging from of the master process
+            if self.gpu_id == 0 and self.batch_passes != 0:
                 
                 # print loss terms
                 loss_message_parts = [f'batch_index: {self.batch_passes-1} | loss_avg: {loss_avg.item():.4f}']
@@ -123,8 +142,9 @@ class ModelTrainer(Thread):
             if self.training_buffer.openpmdProduction == False:
                 print(f"Note: The streaming has stopped, the trainer will run for "
                         f"{self.ts_after_stopped_production} training steps (batch passes) "
-                        "before stopping.\n" 
-                         f"Training step:{rest_training_left_counter} after the streaming has stopped.")
+                        f"before stopping.\n"
+                        f"Training step:{rest_training_left_counter} after the streaming has stopped.")
                 rest_training_left_counter+=1
                 if rest_training_left_counter>self.ts_after_stopped_production:
+                    if self.multigpu_run is not None: dist.destroy_process_group()
                     break
