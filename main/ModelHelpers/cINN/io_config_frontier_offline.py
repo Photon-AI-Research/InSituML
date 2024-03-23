736e45dd344899cf55e25899623967771630e78d
Jeffrey Kelling <j.kelling@hzdr.de>
2024-03-23 18:39:49 +0100

    Add io_config_frontier_offline.py

---
+++ main/ModelHelpers/cINN/io_config_frontier_offline.py
@@ -0,0 +1,43 @@
+import pathlib
+
+modelPathPattern = str(pathlib.Path(__file__).parent.resolve()) + '/trained_models/{}/best_model_'
+
+#######################################
+## openPMD data loader configuration ##
+#######################################
+ps_dims = 6 # Actually used in the model configuration by now
+            # ToDo: Use in StreamingLoader
+
+number_of_particles = 4000
+
+normalization_values = dict(
+    momentum_mean = 0.,
+    momentum_std = 1.,
+    force_mean = 0.,
+    force_std = 1.,
+)
+
+streamLoader_config = dict(
+    t0 = 890,
+    t1 = 898, # endpoint=false, t1 is not used in training
+    streaming_config = None, # set to None when reading from file
+    pathpattern1 = "/lustre/orion/csc380/world-shared/ksteinig/002_KHI_withRad_randomInit_data-subset/openPMD/simData_%T.bp", # files on frontier
+    pathpattern2 = "/lustre/orion/csc380/world-shared/ksteinig/002_KHI_withRad_randomInit_data-subset/radiationOpenPMD/e_radAmplitudes%T.bp", # files on frontier
+    amplitude_direction=0, # choose single direction along which the radiation signal is observed, max: N_observer-1, where N_observer is defined in PIConGPU's radiation plugin
+    phase_space_variables = ["momentum", "force"], # allowed are "position", "momentum", and "force". If "force" is set, "momentum" needs to be set too.
+    normalization = normalization_values,
+    number_particles_per_gpu = 1000
+    ## offline training params
+    num_epochs = .25
+)
+
+openPMD_queue_size=4
+
+trainBatchBuffer_config = dict(
+    continual_bs=4,
+    stall_loader=False,
+    consume_size=4,
+)
+
+runner="srun"
+type_streamer="offline"
