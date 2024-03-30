c51ed4cbbac72630129f009014f0a8d1adf8aa02
Jeffrey Kelling <j.kelling@hzdr.de>
2024-03-30 16:41:43 +0100

    Add trainingBuffer_config.min_tb_from_unchanged_now_bf
    configure hemera for continual rep-learning
    switch to old chamfer's (may somewhat work in linear regime)

--- main/ModelHelpers/cINN/model_config.py
+++ main/ModelHelpers/cINN/model_config.py
@@ -31,13 +31,17 @@ ndim_z = 32,
 load_model = None, #'inn_vae_latent_544_sim007_24k0zbm4/best_model_',
 load_model_checkpoint = 'inn_vae_latent_544_sim014_859eopan/model_150', #'inn_vae_latent_544_sim014_859eopan/model_950',
     
+#   "earthmovers"
+#   "chamfersloss"
+#   "chamfersloss_d"
+#   "chamfersloss_0"
 ## for optimized chamfer distance
-# loss_function = 'chamfersloss_o',
-# loss_kwargs = {'property_':'momentum_force'},
+loss_function = 'chamfersloss',
+loss_kwargs = {},
 
 ## for emd without peops library.
-loss_function = 'earthmovers',
-loss_kwargs = {},
+# loss_function = 'earthmovers',
+# loss_kwargs = {},
 
 betas = (0.8, 0.9),
 eps = 1e-6,
