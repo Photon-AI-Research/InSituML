b682bb7b3434fd314ca0c17a3dacdc80cd2b29c4
jeyhun1 <jeyhun.rustamov@mailbox.tu-dresden.de>
2024-05-24 14:10:17 +0200

    update create_metrics.sh

--- scripts/create_metrics.sh
+++ scripts/create_metrics.sh
@@ -20,15 +20,14 @@ export MASTER_ADDR=$master_addr
 echo HOSTNAME $HOSTNAME
 nvidia-smi
 
-# Usage: sbatch create_metrics.sh [arg1] [arg2] [arg3] [arg4]
+# Usage: sbatch create_metrics.sh [arg1] [arg2] [arg3]
 
 # particle_pathpattern = "/bigdata/hplsim/aipp/SC24_PIConGPU-Continual-Learning/24-nodes_full-picongpu-data/04-01_1013/simOutput/openPMD/simData_%T.bp5",
 # radiation_pathpattern = "/bigdata/hplsim/aipp/SC24_PIConGPU-Continual-Learning/24-nodes_full-picongpu-data/04-01_1013/simOutput/radiationOpenPMD/e_radAmplitudes_%T.bp5"
 
 # Assign arguments to variables with default values
-model_filepath_pattern=${1:-"/bigdata/hplsim/scratch/kelling/chamfers/slurm-6923925/{}"}
-load_model_checkpoint=${2:-"model_24211"}
-particle_pathpattern=${3:-"/bigdata/hplsim/aipp/SC24_PIConGPU-Continual-Learning/04-01_rerun-independent-AE-scaling_chamfersdistance_fix-gpu-volume_scaling/8-nodes_lr-0.0001_min-tb-4_lrAE-20/04-01_1645/simOutput/openPMD/simData_%T.bp5"}
-radiation_pathpattern=${4:-"/bigdata/hplsim/aipp/SC24_PIConGPU-Continual-Learning/04-01_rerun-independent-AE-scaling_chamfersdistance_fix-gpu-volume_scaling/8-nodes_lr-0.0001_min-tb-4_lrAE-20/04-01_1645/simOutput/streamedRadiation/ts_{}.npy"}
+load_model_checkpoint=${1:-"/bigdata/hplsim/scratch/kelling/chamfers/slurm-6923925/model_24211"}
+particle_pathpattern=${2:-"/bigdata/hplsim/aipp/SC24_PIConGPU-Continual-Learning/04-01_rerun-independent-AE-scaling_chamfersdistance_fix-gpu-volume_scaling/8-nodes_lr-0.0001_min-tb-4_lrAE-20/04-01_1645/simOutput/openPMD/simData_%T.bp5"}
+radiation_pathpattern=${3:-"/bigdata/hplsim/aipp/SC24_PIConGPU-Continual-Learning/04-01_rerun-independent-AE-scaling_chamfersdistance_fix-gpu-volume_scaling/8-nodes_lr-0.0001_min-tb-4_lrAE-20/04-01_1645/simOutput/streamedRadiation/ts_{}.npy"}
 
-mpirun -n 1 python $INSITUML/tools/calculate_metrics.py --model_filepath_pattern $model_filepath_pattern --load_model_checkpoint $load_model_checkpoint --particle_pathpattern $particle_pathpattern --radiation_pathpattern $radiation_pathpattern
+mpirun -n 1 python $INSITUML/tools/calculate_metrics.py --load_model_checkpoint $load_model_checkpoint --particle_pathpattern $particle_pathpattern --radiation_pathpattern $radiation_pathpattern
