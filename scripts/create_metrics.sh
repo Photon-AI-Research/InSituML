7d09abbdd5010f2703fea559b1dc268206fc042a
jeyhun1 <jeyhun.rustamov@mailbox.tu-dresden.de>
2024-05-24 13:06:50 +0200

    update create_metrics.sh

--- scripts/create_metrics.sh
+++ scripts/create_metrics.sh
@@ -8,28 +8,27 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:1
 
-# base_path="/bigdata/hplsim/aipp/SC24_PIConGPU-Continual-Learning/03-30_learning-rate-scaling-with-ranks_chamfersdistance_fix-gpu-volume/96-nodes_lr-0.0005_min-tb-16"
-base_path="/bigdata/hplsim/scratch/kelling/chamfers/slurm-6923925"
-# base_path="/bigdata/hplsim/aipp/SC24_PIConGPU-Continual-Learning/04-01_rerun-independent-AE-scaling_chamfersdistance_fix-gpu-volume_scaling/8-nodes_lr-0.0001_min-tb-4_lrAE-20/04-01_1840"
-# base_path="trained_models"
-#/bigdata/hplsim/scratch/kelling/chamfers/slurm-6923925
+export INSITUML="${INSITUML:-.}"
 
-# the plot directory path
-plot_directory_path="/bigdata/hplsim/aipp/SC24_PIConGPU-Continual-Learning/metrics_and_plots/plots_slurm-6923925/$SLURM_JOB_ID"
+. $INSITUML/share/env/ddp_tested_hemera_env.sh
 
-# create the directory after the job starts
-mkdir -p $plot_directory_path
+export WORLD_SIZE=$SLURM_NTASKS
+export MASTER_PORT="1$( echo -n $SLURM_JOBID | tail -c 4 )"
+master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$master_addr
 
-# Load modules
-module load python gcc git gcc/12.2.0 cuda/12.1 openmpi/4.1.5-cuda121-gdr
-module load zlib/1.2.11 libfabric/1.17.0 c-blosc2 hdf5-parallel/1.12.0-omp415-cuda121 adios2/2.9.2-cuda121-blosc2 libpng/1.6.39
+echo HOSTNAME $HOSTNAME
+nvidia-smi
 
-# Activate Python environment
-source /home/rustam75/ml_env/ml_stream/bin/activate
+# Usage: sbatch create_metrics.sh [arg1] [arg2] [arg3] [arg4]
 
-# Change to the desired directory
-cd /home/rustam75/InSituML/main/ModelHelpers/cINN/
+# particle_pathpattern = "/bigdata/hplsim/aipp/SC24_PIConGPU-Continual-Learning/24-nodes_full-picongpu-data/04-01_1013/simOutput/openPMD/simData_%T.bp5",
+# radiation_pathpattern = "/bigdata/hplsim/aipp/SC24_PIConGPU-Continual-Learning/24-nodes_full-picongpu-data/04-01_1013/simOutput/radiationOpenPMD/e_radAmplitudes_%T.bp5"
 
-# python calculate_metrics.py --sim "24-nodes_full-picongpu-data" --N_samples 5 --eval_timesteps 900 950 1000 --generate_best_box_plot True --generate_plots True --plot_directory_path "$plot_directory_path" --model_filepath_pattern "${base_path}/{}" --load_model_checkpoint "model_24000"
+# Assign arguments to variables with default values
+model_filepath_pattern=${1:-"/bigdata/hplsim/scratch/kelling/chamfers/slurm-6923925/{}"}
+load_model_checkpoint=${2:-"model_24211"}
+particle_pathpattern=${3:-"/bigdata/hplsim/aipp/SC24_PIConGPU-Continual-Learning/04-01_rerun-independent-AE-scaling_chamfersdistance_fix-gpu-volume_scaling/8-nodes_lr-0.0001_min-tb-4_lrAE-20/04-01_1645/simOutput/openPMD/simData_%T.bp5"}
+radiation_pathpattern=${4:-"/bigdata/hplsim/aipp/SC24_PIConGPU-Continual-Learning/04-01_rerun-independent-AE-scaling_chamfersdistance_fix-gpu-volume_scaling/8-nodes_lr-0.0001_min-tb-4_lrAE-20/04-01_1645/simOutput/streamedRadiation/ts_{}.npy"}
 
-python calculate_metrics.py --sim "014" --N_samples 5 --eval_timesteps 900 950 1000 --generate_best_box_plot True --generate_plots True --plot_directory_path "$plot_directory_path" --model_filepath_pattern "${base_path}/{}" --load_model_checkpoint "model_24000"
+mpirun -n 1 python $INSITUML/tools/calculate_metrics.py --model_filepath_pattern $model_filepath_pattern --load_model_checkpoint $load_model_checkpoint --particle_pathpattern $particle_pathpattern --radiation_pathpattern $radiation_pathpattern
