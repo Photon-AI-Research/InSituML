04851b82b9502da9b584c6358877dc2668bbf3cb
Jeffrey Kelling <j.kelling@hzdr.de>
2024-08-21 17:37:11 +0200

    io_config_hemera.py: improve batch size scaling...
    Add continual_bs == training_bs (no -1, to allow for proper bs scaling with ddp)
    Add scale min_tb_from_unchanged_now_bf inversely to local batch size

--- share/configs/io_config_hemera.py
+++ share/configs/io_config_hemera.py
@@ -58,11 +58,11 @@ out_prefix = (
 
 trainBatchBuffer_config = dict(
     training_bs=batch_size,
-    continual_bs=batch_size
-    - 1,  # 7 is the max we can fit on P100 with our stupid chamfer's impl
+    continual_bs=batch_size,
+    # - 1,  # 7 is the max we can fit on P100 with our stupid chamfer's impl
     stall_loader=True,
     consume_size=1,
-    min_tb_from_unchanged_now_bf=4,
+    min_tb_from_unchanged_now_bf=round(4*4/batch_size),
     radiation_data_folder=out_prefix,
     # Train buffer.
     buffersize=10,
