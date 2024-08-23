b38c4a29924f8c2a7ba2c6d6e406000f0c0950bb
Jeffrey Kelling <j.kelling@hzdr.de>
2024-08-23 20:29:05 +0200

    Add config for frontier offline

--- share/configs/io_config_frontier_offline.py
+++ share/configs/io_config_frontier_offline.py
@@ -14,20 +14,16 @@ ps_dims = 6  # Actually used in the model configuration by now
 number_of_particles = 4096
 
 streamLoader_config = dict(
-    t0=898,
+    t0=900,
     t1=999,  # endpoint=false, t1 is not used in training
     streaming_config=None,  # set to None when reading from file
     particle_pathpattern=(
-        "/lustre/orion/csc380/proj-shared/ksteinig/"
-        + "2024-03_Training-from-Stream_chamfersdistance_fix"
-        + "-gpu-volume_scaling/24-nodes_full-picongpu-data/"
+        "/lustre/orion/csc621/proj-shared/kelling/"
         + "04-01_1013/simOutput/openPMD/simData_%T.bp5"
     ),
     # files for 96GCDs on frontier
     radiation_pathpattern=(
-        "/lustre/orion/csc380/proj-shared/ksteinig/"
-        + "2024-03_Training-from-Stream_chamfersdistance_fix"
-        + "-gpu-volume_scaling/24-nodes_full-picongpu-data/"
+        "/lustre/orion/csc621/proj-shared/kelling/"
         + "04-01_1013/simOutput/radiationOpenPMD/"
         + "e_radAmplitudes_%T.bp5"
     ),  # files for 96GCDs on frontier
@@ -48,22 +44,21 @@ streamLoader_config = dict(
     number_particles_per_gpu=30000,
     verbose=False,
     # offline training params
-    num_epochs=0.25,
+    num_epochs=2,
 )
 
-openPMD_queue_size = 4
+openPMD_queue_size = 8
 
-batch_size = 4
+batch_size = int(environ["BATCH_SIZE"]) if "BATCH_SIZE" in environ else 8
+
 trainBatchBuffer_config = dict(
     training_bs=batch_size,
-    continual_bs=batch_size - 1,
+    continual_bs=batch_size,
+    # - 1,  # 7 is the max we can fit on P100 with our stupid chamfer's impl
     stall_loader=True,
     consume_size=1,
-    min_tb_from_unchanged_now_bf=(
-        int(environ["MIN_TB_FROM_UNCHANGED_NOW_BF"])
-        if ("MIN_TB_FROM_UNCHANGED_NOW_BF" in environ)
-        else 2
-    ),
+    min_tb_from_unchanged_now_bf=round(int(environ.get("ISML_MIN_TB_FROM_UNCHANGED", 32))*2/batch_size),
+    radiation_data_folder=out_prefix,
     radiation_data_folder="streamedRadiation",  # fixed
     # Train buffer.
     buffersize=10,
